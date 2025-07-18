e578a1fdef40ae3faa4d5f553baf27c1fd2ac8b8
Christopher Bicknell <chrisb2812@gmail.com>
2025-07-18 10:34:21 +0200

    design revamp

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,20 @@ import '../styles/header.css'
 
 export default function Header() {
 return (
+    
         <header>
-            <div>
-            <h1>Welcome to My Portfolio</h1>
-            <p>Web Developer | Designer | Problem Solver</p>
-            </div>
+            <nav>
+                <div className="gradient-background">
+                    <h1 className="logo">bickrs.com</h1>
+                    <ul className="nav-links">
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/about">About</a></li>
+                        <li><a href="/contact">Contact</a></li>
+                    </ul>
+                </div>
+            </nav>
         </header>
+    
+        
     )
 }
