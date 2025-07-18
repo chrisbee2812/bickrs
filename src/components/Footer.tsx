f25a62f9a68224a474d15a8c91c43682f1e74451
Christopher Bicknell <chrisb2812@gmail.com>
2025-07-18 11:20:33 +0200

    icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,13 +6,13 @@ export default function Footer() {
         
         <div>
             <a href="mailto:chris@bickrs.com" id="email-link">
-                <img src="../images/envelope.svg" alt="Email Icon" />
+                <img className="icons" src="../images/envelope.svg" alt="Email Icon" />
             </a>
             <a href="https://www.linkedin.com/in/chris-bicknell-69a9a136/" target="_blank" id="linkedin-link">
-                <img src="../images/linkedin.svg" alt="LinkedIn Icon" />
+                <img className="icons" src="../images/linkedin.svg" alt="LinkedIn Icon" />
             </a>
             <a href="https://github.com/chrisbee2812/" target="_blank" id="github-link">
-                <img src="../images/github.svg" alt="GitHub Icon" />
+                <img className="icons" src="../images/github.svg" alt="GitHub Icon" />
             </a>
         </div>
         
