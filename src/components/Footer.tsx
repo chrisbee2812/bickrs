bf11bb701cec86de1038a2b30e73533f4fa3331f
Christopher Bicknell <chrisb2812@gmail.com>
2025-07-18 17:20:24 +0200

    text wrapper

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,22 +1,22 @@
 import '../styles/footer.css'
+import envelope from '../images/envelope.svg'
+import linkedin from '../images/linkedin.svg'
+import github from '../images/github.svg'
 
 export default function Footer() {
     return (
-    <footer>
-        
+    <footer>        
         <div>
             <a href="mailto:chris@bickrs.com" id="email-link">
-                <img className="icons" src="../images/envelope.svg" alt="Email Icon" />
+                <img className="icons" src={envelope} alt="Email Icon" />
             </a>
             <a href="https://www.linkedin.com/in/chris-bicknell-69a9a136/" target="_blank" id="linkedin-link">
-                <img className="icons" src="../images/linkedin.svg" alt="LinkedIn Icon" />
+                <img className="icons" src={linkedin} alt="LinkedIn Icon" />
             </a>
             <a href="https://github.com/chrisbee2812/" target="_blank" id="github-link">
-                <img className="icons" src="../images/github.svg" alt="GitHub Icon" />
+                <img className="icons" src={github} alt="GitHub Icon" />
             </a>
         </div>
-        
-        
     </footer>
     )
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
+import React, { useEffect, useState } from 'react'
 import '../styles/main.css'
 
 export const Route = createFileRoute('/')({
@@ -6,11 +7,36 @@ export const Route = createFileRoute('/')({
 })
 
 function App() {
+
+ 
+    const texts = [
+      <h2><span className="span1">Website Developer</span></h2>,
+      <h2><span className="span1">Website Designer</span></h2>, 
+      <h2><span className="span1">JavaScript Developer</span></h2>, 
+      <h2><span className="span1">React Developer</span></h2>, 
+      <h2><span className="span1">Father</span></h2>
+    ]
+
+    function textSwapper() {
+      const [currentText, setCurrentText] = useState(texts[0]);
+      useEffect(() => {
+        const intervalId = setInterval(() => {
+          setCurrentText(prevText => {
+            const currentIndex = texts.indexOf(prevText);
+            const nextIndex = (currentIndex + 1) % texts.length;
+            return texts[nextIndex];
+          });
+        }, 4000); // Change text every 4 seconds
+        return () => clearInterval(intervalId);
+      }, []);
+      return currentText;
+    }
+
   return (
     <main>
       <div className="hero">      
         <h1>HI, I'M CHRIS</h1>
-        <h2>WebSite Developer</h2>
+        {textSwapper()}
       </div>
     </main>
   )
