df498aff990be7dc0fdfbdcb0cc0a628cadd46e3
Christopher Bicknell <chrisb2812@gmail.com>
2025-07-20 16:59:47 +0200

    retry as build failed

--- src/routes/projects/project1.tsx
+++ src/routes/projects/project1.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute, Link } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/projects/project1')({
   component: RouteComponent,
