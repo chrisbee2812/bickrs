d5f8adede12c0dd3d627ed31b0d675ad0b1c64bb
Christopher Bicknell <chrisb2812@gmail.com>
2025-07-20 14:39:04 +0200

    reconfig for ionos...

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,41 @@
+// import { defineConfig } from 'vite'
+// import viteReact from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+// import { resolve } from 'node:path'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     TanStackRouterVite({ autoCodeSplitting: true }),
+//     viteReact(),
+//     tailwindcss(),
+//   ],
+//   test: {
+//     globals: true,
+//     environment: 'jsdom',
+//   },
+//   resolve: {
+//     alias: {
+//       '@': resolve(__dirname, './src'),
+//     },
+//   },
+// })
+
 import { defineConfig } from 'vite'
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import { resolve } from 'node:path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    TanStackRouterVite({ autoCodeSplitting: true }),
-    viteReact(),
-    tailwindcss(),
+    react(),
+    
   ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
+  base: './',
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  },
+      '~': '/src'
+    }
+  }
 })
