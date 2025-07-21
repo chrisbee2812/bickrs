9707f2725a70e8ae7fef6bfc8cf8459875dc7ea4
Christopher Bicknell <chrisb2812@gmail.com>
2025-07-21 13:23:38 +0200

    fixing build issue for testing

--- src/routes/projects/route.tsx
+++ src/routes/projects/route.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute, Link, Outlet, useRouterState } from '@tanstack/react-router'
+import { createFileRoute, Link, useRouterState } from '@tanstack/react-router'
 import '../../styles/projects/projects.css'
 
 export const Route = createFileRoute('/projects')({
